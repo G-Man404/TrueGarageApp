35c2b3c4a4d088fe35f7f8ee3fb7eb2e6cea71ad
gman <anisimovf94@gmail.com>
2024-04-15 13:05:06 +0300

    v 0.9.6

--- admin_app/admin.py
+++ admin_app/admin.py
@@ -1,6 +1,8 @@
 import os
 import tempfile
 
+from django.shortcuts import render
+from django.urls import path
 from docxtpl import DocxTemplate
 from openpyxl import Workbook
 
@@ -10,28 +12,31 @@ from django.http import HttpResponse
 from .models import Order, Task, Supplies, Client, Engineer, Motorcycle, Work, Supply, User
 
 
-def get_full_price(order: Order):
-    full_price = 0
-    for task in order.task_set.all():
-        full_price += task.work.price
-    for supply in order.supplies_set.all():
-        full_price += supply.supply.price * supply.count
-    return full_price
-
-
 def print_order(modeladmin, request, queryset):
     for obj in queryset:
         response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
         response['Content-Disposition'] = f'attachment; filename="file_{obj.id}.docx"'
+        full_task_price = sum([task.work.price * task.count for task in obj.task_set.all()])
+        full_supply_price = sum([supply.supply.price * supply.count for supply in obj.supplies_set.all()])
         context = {
             "client": obj.client,
             "motorcycle": obj.motorcycle,
             "tasks": obj.task_set.all(),
             "supplies": obj.supplies_set.all(),
-            "full_price": get_full_price(obj),
-            "discount": obj.discount,
+            "full_task_price": full_task_price,
+            "full_task_price_with_discount": full_task_price * (1 - obj.task_discount / 100),
+            "full_supply_price": full_supply_price,
+            "full_supply_price_with_discount": full_supply_price * (1 - obj.supply_discount / 100),
+            "task_discount": obj.task_discount,
+            "supply_discount": obj.supply_discount,
+            "deposit": obj.deposit,
+            "full_price_with_discount": (full_task_price * (1 - obj.task_discount / 100)) +
+                                        (full_supply_price * (1 - obj.supply_discount / 100)),
+            "full_price_with_deposit": (full_task_price * (1 - obj.task_discount / 100)) +
+                                       (full_supply_price * (1 - obj.supply_discount / 100)) - obj.deposit,
             "number": obj.number,
             "created_at": obj.created_at.date(),
+
         }
         file = DocxTemplate("templates/template.docx")
         file.render(context)
@@ -92,16 +97,38 @@ class EngineerAdmin(admin.ModelAdmin):
 
 class MotorcycleAdmin(admin.ModelAdmin):
     actions = [export_to_excel]
+
     list_display = ['name', 'model', 'owner', 'mfg_year', 'state_number', 'vin']
 
 
 class OrderAdmin(admin.ModelAdmin):
+    change_form_template = 'admin_app/order_change_form.html'
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        order = Order.objects.get(pk=object_id)
+        extra_context['total_work_price'] = sum(task.work.price * task.count for task in order.task_set.all())
+        extra_context['total_work_price_with_discount'] = (extra_context['total_work_price'] *
+                                                           (1 - order.task_discount / 100))
+        extra_context['total_supply_price'] = sum(
+            supply.supply.price * supply.count for supply in order.supplies_set.all())
+        extra_context['total_supply_price_with_discount'] = (extra_context['total_supply_price'] *
+                                                             (1 - order.supply_discount / 100))
+
+        return super().change_view(
+            request, object_id, form_url, extra_context=extra_context,
+        )
+
+    def engineers_list(self, obj):
+        return ", ".join([str(engineer.user) for engineer in obj.engineers.all()])
+    engineers_list.short_description = 'Engineers'
+
     inlines = [
         TaskInline,
         SuppliesInline,
     ]
-    list_display = ['number', 'created_at', 'client', 'engineer', 'motorcycle', 'status']
-    list_filter = ['status', 'engineer']
+    list_display = ['number', 'created_at', 'client', 'engineers_list', 'motorcycle', 'status']
+    list_filter = ['status', 'engineers']
     search_fields = ['number', "client__user__first_name", "engineer__user__first_name",
                      'motorcycle__vin', 'motorcycle__state_number']
     exclude = ["number", ]
--- admin_app/models.py
+++ admin_app/models.py
@@ -4,13 +4,13 @@ from django.db import models
 
 class User(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
-    phone_number = models.CharField(max_length=15)
-    email = models.EmailField()
+    phone_number = models.CharField(max_length=15, blank=True)
+    email = models.EmailField(blank=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
 
     def __str__(self):
-        return f"{self.first_name} {self.first_name}"
+        return f"{self.first_name} {self.last_name}"
 
 
 class Client(models.Model):
@@ -78,8 +78,6 @@ class Supply(models.Model):
         return self.name
 
 
-
-
 class Order(models.Model):
     number = models.CharField(max_length=20, default=None)
     created_at = models.DateTimeField(default=timezone.now)
@@ -88,17 +86,17 @@ class Order(models.Model):
         on_delete=models.CASCADE,
         related_name="ClientList"
     )
-    engineer = models.ForeignKey(
-        Engineer,
-        on_delete=models.CASCADE,
-        related_name="Engineer"
+    engineers = models.ManyToManyField(
+        Engineer
     )
     motorcycle = models.ForeignKey(
         Motorcycle,
         on_delete=models.CASCADE,
         related_name="Motorcycle"
     )
-    discount = models.FloatField(default=0)
+    deposit = models.FloatField(default=0, verbose_name="Предоплата (Руб.):")
+    task_discount = models.FloatField(default=0, verbose_name="Скидка на работы (%):")
+    supply_discount = models.FloatField(default=0, verbose_name="Скидка на запчасти (%):")
     description = models.TextField(blank=True)
     comments = models.TextField(blank=True)
     status = models.CharField(
@@ -129,6 +127,7 @@ class Task(models.Model):
         Work,
         on_delete=models.CASCADE
     )
+    count = models.FloatField(default=1)
     status = models.CharField(
         choices=(
             ("in_queue", "В очереди"),
